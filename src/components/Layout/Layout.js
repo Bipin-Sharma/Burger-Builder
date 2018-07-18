a49fc136918992e68c2974ee217e88f716db9e7d
Bipin Sharma Regmi <bipinsharmaregmi@gmail.com>
2018-07-17 21:03:13 -0500

    Solving Issue
    Solving Issue 1

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,8 +1,13 @@
+import React from 'react';
+import Aux from '../../hoc/Aux';
+
 const layout = (props) => (
-  <div> Toolbar, sideDrawer, Backdrop </div>
-  <main>
-      {props.children}
-  </main>
+    <Aux>
+      <div> Toolbar, sideDrawer, Backdrop </div>
+      <main>
+          {props.children}
+      </main>
+    </Aux>
+);
 
-
-)
+export default layout;
